1296a44716fc60a6723ee0f03e8fa9d0e0819c28
Maciejoss <maciejkordyaczny@gmail.com>
2025-01-04 20:03:35 +0100

    Align to backend

--- Pages/OpenCase.cshtml.cs
+++ Pages/OpenCase.cshtml.cs
@@ -1,5 +1,5 @@
 using Grpc.Net.Client;
-using gRPCServer;
+using GrpcService1;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,43 +7,41 @@ namespace CaseBattleNew.Pages
 {
     public class OpenCaseModel : PageModel
     {
-        public List<CaseItem> CaseItems { get; set; } = new List<CaseItem>();
+        public List<ChestDto> Chests { get; set; } = new();
         public async Task<JsonResult> OnPostOpenCase()
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5051");
-            var client = new CaseOpener.CaseOpenerClient(channel);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5003");
+            var client = new ChestGrpc.ChestGrpcClient(channel);
 
             // Make a request to open a case
-            var response = await client.OpenCaseAsync(new OpenCaseRequest { CaseId = "1" });
-            Console.WriteLine("TEST AAAAAAAAAAAAA" + response.Name);
+            var response = await client.OpenChestAsync(new OpenChestRequest());
+            Console.WriteLine("TEST AAAAAAAAAAAAA" + response.ReceivedItem);
             // Return the result as JSON
             return new JsonResult(new
             {
-                Index = response.Index, // The index of the segment
-                Name = response.Name,   // The name of the selected item
-                ImageUrl = response.ImageUrl // Optional, if you want to display an image
+                Index = response.ReceivedItem.Id, // The index of the segment
+                Name = response.ReceivedItem.Name ,   // The name of the selected item
+                ImageUrl = response.ReceivedItem.ImageUrl // Optional, if you want to display an image
             });
         }
         public async Task OnGet()
         {
-        
-                using var channel = GrpcChannel.ForAddress("https://localhost:5051");
-                var client = new CaseOpener.CaseOpenerClient(channel);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5003");
+            var client = new ChestGrpc.ChestGrpcClient(channel);
 
                 // Make a request to open a case
-                var response = await client.GetCaseItemsAsync(new GetCaseItemsRequest { CaseId = "2" });
+                var response = await client.GetChestsAsync(new GetChestsRequest());
 
-                CaseItems = response.Items.ToList();
+                var items = response.Chests.FirstOrDefault()!.PossibleItems.ToList();
                 Console.WriteLine("Retrieved Items:");
-                foreach (var item in response.Items)
+                foreach (var item in items)
                 {
-                    Console.WriteLine($"Name: {item.Name}");
-                    Console.WriteLine($"Image URL: {item.ImageUrl}");
+                    Console.WriteLine($"Name: {item.Item.Name}");
+                    Console.WriteLine($"Image URL: {item.Item.ImageUrl}");
                     Console.WriteLine("------------------------");
                 }
 
-            Console.WriteLine("Hej"+CaseItems[0].Name);
-            
+                Chests = response.Chests.ToList();
         }
     }
 }
