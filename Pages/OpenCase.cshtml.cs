0f737221b1672077496d650c9d793cbe0d6a9def
pokermonik <pokermonik200@gmail.com>
2025-01-06 23:11:56 +0100

    +Open case fix, now properly works with both proto and database

--- Pages/OpenCase.cshtml.cs
+++ Pages/OpenCase.cshtml.cs
@@ -7,41 +7,41 @@ namespace CaseBattleNew.Pages
 {
     public class OpenCaseModel : PageModel
     {
-        public List<ChestDto> Chests { get; set; } = new();
+        public List<ItemDto> CaseItems { get; set; } = new List<ItemDto>();
         public async Task<JsonResult> OnPostOpenCase()
         {
             using var channel = GrpcChannel.ForAddress("http://localhost:5003");
             var client = new ChestGrpc.ChestGrpcClient(channel);
 
             // Make a request to open a case
-            var response = await client.OpenChestAsync(new OpenChestRequest());
+            var response = await client.OpenChestAsync(new OpenChestRequest{ UserId = 1, ChestId = 2 });
             Console.WriteLine("TEST AAAAAAAAAAAAA" + response.ReceivedItem);
             // Return the result as JSON
             return new JsonResult(new
             {
                 Index = response.ReceivedItem.Id, // The index of the segment
-                Name = response.ReceivedItem.Name ,   // The name of the selected item
+                Name = response.ReceivedItem.Name,   // The name of the selected item
                 ImageUrl = response.ReceivedItem.ImageUrl // Optional, if you want to display an image
             });
         }
         public async Task OnGet()
         {
             using var channel = GrpcChannel.ForAddress("http://localhost:5003");
-            var client = new ChestGrpc.ChestGrpcClient(channel);
+            var client = new ItemGrpc.ItemGrpcClient(channel);
 
                 // Make a request to open a case
-                var response = await client.GetChestsAsync(new GetChestsRequest());
-
-                var items = response.Chests.FirstOrDefault()!.PossibleItems.ToList();
+                var response = await client.GetChestItemsAsync(new GetChestItemsRequest { ChestId=2});
+            
+                var items = response.Items.ToList();
                 Console.WriteLine("Retrieved Items:");
                 foreach (var item in items)
                 {
-                    Console.WriteLine($"Name: {item.Item.Name}");
-                    Console.WriteLine($"Image URL: {item.Item.ImageUrl}");
+                    Console.WriteLine($"Name: {item.Name}");
+                    Console.WriteLine($"Image URL: {item.ImageUrl}");
                     Console.WriteLine("------------------------");
                 }
-
-                Chests = response.Chests.ToList();
+            CaseItems = items;
+                
         }
     }
 }
